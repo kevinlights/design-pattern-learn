4aa39b0d5873d4b8c0c0e366e07ccdfb72a6459b
guangjh <guangjinhai@163.com>
2022-03-12 19:58:47 +0800

    mediator pattern;增加新的同事类;

--- mediator-pattern/src/main/java/sample/Mediator.java
+++ mediator-pattern/src/main/java/sample/Mediator.java
@@ -8,25 +8,29 @@ package sample;
 class Client {
     public static void main(String[] args) {
         // 中介者
-        ConcreteMediator mediator = new ConcreteMediator();
+        // ConcreteMediator mediator = new ConcreteMediator();
+        SubConcreteMediator mediator = new SubConcreteMediator();
 
         //定义同事对象
         Button addBT = new Button();
         List list = new List();
         ComboBox cb = new ComboBox();
         TextBox userNameTB = new TextBox();
+        Label label = new Label();
 
         // 同事对象关联中介者
         addBT.setMediator(mediator);
         list.setMediator(mediator);
         cb.setMediator(mediator);
         userNameTB.setMediator(mediator);
+        label.setMediator(mediator);
 
         // 中介对象关联同事对象
         mediator.addButton = addBT;
         mediator.list = list;
         mediator.cb = cb;
         mediator.userNameTextBox = userNameTB;
+        mediator.label = label;
 
         // 同事对象更新
         addBT.changed();
@@ -79,6 +83,34 @@ class ConcreteMediator extends Mediator {
     }
 }
 
+class SubConcreteMediator extends ConcreteMediator {
+    public Label label;
+
+    @Override
+    public void componentChanged(Component c) {
+        //单击按钮
+        if(c == addButton) {
+            System.out.println("--单击增加按钮--");
+            list.update();
+            cb.update();
+            userNameTextBox.update();
+            label.update(); //文本标签更新
+        }
+        //从列表框选择客户
+        else if(c == list) {
+            System.out.println("--从列表框选择客户--");
+            cb.select();
+            userNameTextBox.setText();
+        }
+        //从组合框选择客户
+        else if(c == cb) {
+            System.out.println("--从组合框选择客户--");
+            cb.select();
+            userNameTextBox.setText();
+        }
+    }
+}
+
 class Button extends Component {
 
     @Override
@@ -120,5 +152,12 @@ class ComboBox extends Component {
 
     public void select() {
         System.out.println("组合框选中项：小龙女。");
+    }
+}
+
+class Label extends Component {
+    @Override
+    public void update() {
+        System.out.println("文本标签内容改变，客户信息总数加1。");
     }
 }
