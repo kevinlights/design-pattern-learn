9c24e867b8dcdb15326fa26750830ae8c580918e
guangjh <guangjinhai@163.com>
2022-03-13 09:53:44 +0800

    facade pattern;abstract facade;

--- facade-pattern/src/main/java/sample/EncryptFacade.java
+++ facade-pattern/src/main/java/sample/EncryptFacade.java
@@ -1,5 +1,7 @@
 package sample;
 
+import dp.utils.XmlUtil;
+
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
@@ -9,13 +11,29 @@ import java.util.Base64;
  * @author kevinlights
  */
 class Client {
-    public static void main(String[] args) {
-        EncryptFacade facade = new EncryptFacade();
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        AbstractEncryptFacade facade = (AbstractEncryptFacade) Class.forName(XmlUtil.getL1Value("config.xml", "facade")).newInstance();
         facade.fileEncrypt("src.txt", "target.txt");
     }
 }
 
-public class EncryptFacade {
+abstract class AbstractEncryptFacade {
+    public abstract void fileEncrypt(String sourceFile, String targetFile);
+}
+
+class NewEncryptFacade extends AbstractEncryptFacade {
+    private FileReader reader = new FileReader();
+    private FileWriter writer = new FileWriter();
+    private NewCipherMachine cipher = new NewCipherMachine();
+
+    public void fileEncrypt(String sourceFile, String targetFile) {
+        String plainText = reader.read(sourceFile);
+        String encryped = cipher.encrypt(plainText);
+        writer.write(encryped, targetFile);
+    }
+}
+
+public class EncryptFacade extends AbstractEncryptFacade {
     private FileReader reader = new FileReader();
     private FileWriter writer = new FileWriter();
     private CipherMachine cipher = new CipherMachine();
@@ -40,6 +58,12 @@ class FileWriter {
 }
 
 class CipherMachine {
+    public String encrypt(String plainText) {
+        return Base64.getEncoder().encodeToString(plainText.getBytes(StandardCharsets.UTF_8));
+    }
+}
+
+class NewCipherMachine {
     public String encrypt(String plainText) {
         return Base64.getEncoder().encodeToString(plainText.getBytes(StandardCharsets.UTF_8));
     }
